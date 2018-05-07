835f6179178301cf6b73b1599294dac6fb3d70be
Michael Stegeman <michael@stegeman.me>
2018-05-07 11:01:05 -0600

    Add Travis CI support.

---
+++ travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# clone the webthing-tester
+git clone https://github.com/mozilla-iot/webthing-tester
+pip3 install --user -r webthing-tester/requirements.txt
+
+# build and test the single-thing example
+node example/single-thing.js &
+EXAMPLE_PID=$!
+sleep 5
+./webthing-tester/test-client.py
+kill -15 $EXAMPLE_PID
+
+# build and test the multiple-things example
+node example/multiple-things.js &
+EXAMPLE_PID=$!
+sleep 5
+./webthing-tester/test-client.py --path-prefix "/0"
+kill -15 $EXAMPLE_PID
