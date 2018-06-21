59f03d8ee42593e4bb0a966c43dd562d0d3f4abc
Philippe Coval <rzr@users.sf.net>
2018-06-21 20:02:06 +0200

    example: Add simplest actuator example (#16)
    * example: Add simplest actuator example
    
    It can used using:
    
    curl -X PUT -H 'Content-Type: application/json' --data '{"on": true }' \
      http://localhost:8888/properties/on
    
    This base was helpful to port to alternate runtimes (ie: IoT.js)
    
    More details and demo video at:
    
    https://s-opensource.org/webthing-iotjs/
    
    Change-Id: Ie126c5621b18afd0094ee21b06501b22a726974a
    Origin: https://github.com/tizenteam/webthing-node
    Signed-off-by: Philippe Coval <p.coval@samsung.com>
    
    * Update simplest-thing.js

---
+++ example/simplest-thing.js
@@ -0,0 +1,53 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+/**
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+const {
+  Property,
+  SingleThing,
+  Thing,
+  Value,
+  WebThingServer,
+} = require('../index');
+
+function makeThing() {
+  const thing = new Thing('ActuatorExample',
+                          'onOffSwitch',
+                          'An actuator example that just log');
+
+  thing.addProperty(
+    new Property(thing,
+                 'on',
+                 new Value(true, (update) => console.log(`change: ${update}`)),
+                 {type: 'boolean',
+                  description: 'Whether the output is changed'}));
+  return thing;
+}
+
+function runServer() {
+  const port = process.argv[2] ? Number(process.argv[2]) : 8888;
+  const url = `http://localhost:${port}/properties/on`;
+
+  console.log(`Usage:\n
+${process.argv[0]} ${process.argv[1]} [port]
+
+Try:
+curl -X PUT -H 'Content-Type: application/json' --data '{"on": true }' ${url}
+`);
+
+  const thing = makeThing();
+  const server = new WebThingServer(new SingleThing(thing), port);
+  process.on('SIGINT', () => {
+    server.stop();
+    process.exit();
+  });
+  server.start();
+}
+
+runServer();
